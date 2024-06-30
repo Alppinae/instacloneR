e5a6a18ccd6f29ea87bc5a19bf6fd581a59acdf1
Alppinae <seferalp22@gmail.com>
2024-06-30 13:08:16 +0300

    supabase üzerinden login yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
 import { useEffect, useState } from 'react';
-import { Box, Container } from '@mui/material';
-
-import {InstagramWrapper} from './companents/instagramWrapper';
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { supabase } from './supabase/instance.js';
+import { Box, Container } from '@mui/material';
+import {InstagramWrapper} from './companents/instagramWrapper';
 
-function App() {
-  
-    async function getCountries() {
-      const {data} = await supabase.from("countries").select('');
-      console.log(data);
+
+
+function App() {{
+    const [session, setSession] = useState(null)
+
+    useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
+
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
+
+      return () => subscription.unsubscribe()
+    }, [])
+
+    if (!session) {
+      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
     }
+    else {
+      return (
+        <Container maxWidth="lg" width="100%" sx={{
+          marginTop: "1rem",
+          height:"100%"
+          }}
+        >
+          <Box sx={{flexGrow: 1}} height="100%">
+            <InstagramWrapper />
+          </Box>
+        </Container>
+      )
+    }
+  }
 
-  useEffect(() => {
-    getCountries();
-  }, []);
-  return (
-    <Container maxWidth="lg" width="100%" sx={{
-      marginTop: "1rem",
-      height:"100%"
-      }}
-    >
-      <Box sx={{flexGrow: 1}} height="100%">
-        <InstagramWrapper />
-      </Box>
-    </Container>
-  )
 }
 
 export default App
